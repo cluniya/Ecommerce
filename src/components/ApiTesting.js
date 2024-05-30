54cec8c16441ac94b7bfc64ce6b55d550a8a7cb3
cluniya <chandanluniya1986@gmail.com>
2024-05-30 17:26:30 +0530

    Making apps error proof-handling errors

--- src/components/ApiTesting.js
+++ src/components/ApiTesting.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './ApiTesting.css';
 
 const ApiTesting = () => {
     const [moviesList, setMoviesList] = useState([]);
-    const [isLoading,setIsLoading] = useState(false);
-    
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+    const retryInterval = useRef(null);
+
     const fetchMovieHandler = async () => {
         setIsLoading(true);
         try {
-            const response = await fetch('https://swapi.dev/api/films');
+            const response = await fetch('https://swapi.dev/api/film');
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error('Something went wrong... Retrying');
             }
             const data = await response.json();
             const transferredMovies = data.results.map(movie => ({
@@ -19,17 +21,31 @@ const ApiTesting = () => {
                 openingText: movie.opening_crawl,
                 releaseDate: movie.release_date,
             }));
-            if(response.ok){
-                setIsLoading(false);
-            }
             setMoviesList(transferredMovies);
         } catch (error) {
-            console.error('Error fetching movies:', error);
+            setError(error.message);
+            retryInterval.current = setInterval(fetchMovieHandler, 5000);
         }
+        setIsLoading(false)
     };
 
-    if(isLoading) return <div className='loading'><h2>Loading...</h2></div>;
-    
+    const cancelRetryHandler = () => {
+        clearInterval(retryInterval.current);
+        setError('');
+        setIsLoading(false);
+    };
+
+    if (error !== '') {
+        return (
+            <div className="api-testing">
+                <p>{error}</p>
+                <button onClick={cancelRetryHandler} className="cancel-button">Cancel</button>
+            </div>
+        );
+    }
+
+    if (isLoading) return <div className='loading'><h2>Loading...</h2></div>;
+
     return (
         <div className="api-testing">
             <h2>Movies</h2>
